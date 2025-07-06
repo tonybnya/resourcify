4128439fb43018a1c9debc15bec2bc7f2be59abb
tonybnya <nya.tony2010@gmail.com>
2025-07-06 21:11:22 +0100

    feat: set up the db

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,14 +1,16 @@
 """
 Script Name : database.py
-Description : Describe what this script does
+Description : Sets up the SQLite connection, SQLAlchemy engine, and session maker.
 Usage       : python3 database.py [args]
 Author      : @tonybnya
 """
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-def main():
-    pass
+DATABASE_URL = "sqlite:///./resourcify.db"
 
-
-if __name__ == "__main__":
-    main()
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
